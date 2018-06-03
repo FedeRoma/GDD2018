9364024ceb5c9c5e77f043c98d494b7cf195d5fc
matiasdodero <matiasdodero@gmail.com>
2018-06-03 17:09:27 -0300

    estados
    falta modificar el DER para que lo tome correctamente

---
+++ estados.sql
@@ -0,0 +1,9 @@
+INSERT INTO Estados (est_desc)
+VALUES
+/*1*/('Reserva Correcta'),
+/*2*/('Reserva Modificada'),
+/*3*/('Reserva Cancelada por Recepción'),
+/*4*/('Reserva Cancelada por Cliente'),
+/*5*/('Reserva Cancelada por No-Show'),
+/*6*/('Reserva Con Ingreso')
+GO
