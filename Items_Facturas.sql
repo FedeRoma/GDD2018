e66fa2d5001278db91adece825a0e269cdd27b2a
Maximiliano Leonardo Cardozo <maximilianocardozo@outlook.com>
2018-06-03 22:35:36 -0300

    Migracion Items_Facturas
    Falta facturas para poder hacerlo funcionar

---
+++ Items_Facturas.sql
@@ -0,0 +1,12 @@
+use GD1C2018
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+-- TRASPASO DE DATOS DE gd_esquema.Maestra A Items_Facturas
+PRINT 'Migrando Items_Facturas...'
+INSERT INTO Items_Facturas
+		select consumible_codigo,factura_nro,sum(item_factura_monto),sum(item_factura_cantidad) from gd_esquema.maestra
+	where consumible_codigo is not null
+	group by consumible_codigo, factura_nro
+PRINT 'Items_Facturas Migrados Papaaaa...'
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
