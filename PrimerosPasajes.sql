50644d6a672101ba8a5d9e03e441317e3489e70b
Maximiliano Leonardo Cardozo <maximilianocardozo@outlook.com>
2018-06-03 22:27:18 -0300

    Script Inicio con cambios DER

--- PrimerosPasajes.sql
+++ PrimerosPasajes.sql
@@ -106,8 +106,8 @@ PRINT 'Regimenes Migrados Papaaaa...'
 
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 -- TRASPASO DE DATOS DE gd_esquema.Maestra A Regimen_Hotel
-PRINT 'Migrando Regimen_Hotel..'
-INSERT INTO Regimen_Hotel
+PRINT 'Migrando Regimenes_Hoteles..'
+INSERT INTO Regimenes_Hoteles
 		select distinct (select hot_id from Hoteles where Hotel_Calle=hot_calle and Hotel_Ciudad=hot_ciudad),
 		(select reg_id from Regimenes where  Regimen_Descripcion = reg_desc and reg_precio =Regimen_Precio )
 		from gd_esquema.Maestra
