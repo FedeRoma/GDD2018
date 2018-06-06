871791582e4fbf3488aad6f8b4924449d4f3a36b
Maximiliano Leonardo Cardozo <maximilianocardozo@outlook.com>
2018-06-06 00:02:46 +0000

    Add files via upload

---
+++ NuevasTablas/10- Regimenes_Hoteles.sql
@@ -0,0 +1,15 @@
+use GD1C2018
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+-- TRASPASO DE DATOS DE gd_esquema.Maestra A Regimenes_Hoteles
+PRINT 'Migrando Regimenes_Hoteles...'
+ALTER TABLE Items_Facturas NOCHECK CONSTRAINT ALL
+insert into Regimenes_Hoteles(
+					ryh_hot_id,
+					ryh_reg_id
+					)
+select distinct (select hot_id from Hoteles where Hotel_Calle=hot_calle and Hotel_Ciudad=hot_ciudad),
+				(select reg_id from Regimenes where  Regimen_Descripcion = reg_desc and reg_precio =Regimen_Precio )
+from 		gd_esquema.Maestra
+GO
+PRINT 'Regimenes_Hoteles Migrados Papaaaa...'
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
