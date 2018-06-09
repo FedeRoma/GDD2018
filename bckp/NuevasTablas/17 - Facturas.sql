77dd0a0ed4fb0a5febe66991086c465bb99e8592
Federico <romanofederico5@gmail.com>
2018-06-09 17:00:19 -0300

    Factura Terminada

--- bckp/NuevasTablas/17 - Facturas.sql
+++ bckp/NuevasTablas/17 - Facturas.sql
@@ -1,26 +1,9 @@
-SET IDENTITY_INSERT Facturas ON
-/*
-insert into Facturas(
-				fac_id,
-				fac_cli_documento,
-				fac_tipo_pago,
-				fac_fecha,
-				fac_total,
-				fac_est_res_id,
-				fac_med_id,
-				fac_tar_id,
-				fac_cli_doc_id,
-				fac_cye_id
-				)				
-*/
+use GD1C2018
 
-SET IDENTITY_INSERT Facturas OFF
-
-/*TRABAJO DE FEDE EN FACTURAS*/
 
 --Facturas para clientes
 insert into EN_CASA_ANDABA.Facturas
-select distinct M.Factura_Nro,C.cli_documento,M.Factura_Fecha,M.Factura_Total,E.est_res_id,mp.med_id,null,c.cli_doc_id,null
+select distinct M.Factura_Nro,C.cli_documento,M.Factura_Fecha,M.Factura_Total+E.est_precio,E.est_res_id,mp.med_id,null,c.cli_doc_id,null
 from gd_esquema.Maestra M,
 	EN_CASA_ANDABA.Clientes C,
 	EN_CASA_ANDABA.Estadias E,
@@ -32,11 +15,13 @@ and E.est_res_id = R.res_id
 and MP.med_desc = 'Efectivo'
 and R.res_cli_documento = C.cli_documento
 and R.res_cli_doc_id = C.cli_doc_id
+and M.Consumible_Codigo is null
 order by M.Factura_Nro
+
 
 --Facturas para clientesErrores
 insert into EN_CASA_ANDABA.Facturas
-select distinct M.Factura_Nro,C.cye_documento,M.Factura_Fecha,M.Factura_Total,E.est_res_id,mp.med_id,null,C.cye_doc_id,C.cye_id
+select distinct M.Factura_Nro,C.cye_documento,M.Factura_Fecha,M.Factura_Total+E.est_precio,E.est_res_id,mp.med_id,null,C.cye_doc_id,C.cye_id
 from gd_esquema.Maestra M,
 	EN_CASA_ANDABA.ClientesErrores C,
 	EN_CASA_ANDABA.Estadias E,
@@ -46,18 +31,11 @@ where M.Factura_Nro is not null
 AND M.Reserva_Codigo = R.res_id
 and E.est_res_id = R.res_id
 and MP.med_desc = 'Efectivo'
-and R.res_cli_documento = C.cli_documento
-and R.res_cli_doc_id = C.cli_doc_id
+and R.res_cli_documento = C.cye_documento
+and R.res_cli_doc_id = C.cye_doc_id
+and M.Consumible_Codigo is null
+AND M.Cliente_Nombre = cye_nombre
 order by M.Factura_Nro
 
 
 
-
-
-
-select distinct m1.Factura_Nro
-from gd_esquema.Maestra m1, gd_esquema.Maestra m2 
-where m1.Factura_Nro = m2.Factura_Nro and m1.Cliente_Nombre <> m2.Cliente_Nombre 
-		order by m1.Factura_Nro
-		
-		
