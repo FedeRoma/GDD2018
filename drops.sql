1d0e189a147b07d5d116d9b54e1e7c2c0bbe9709
Pablo J <pablojmanteiga@gmail.com>
2018-06-09 15:41:47 -0300

    scripts inicio y drops

---
+++ drops.sql
@@ -0,0 +1,55 @@
+--DROPs
+drop table EN_CASA_ANDABA.Items_Facturas
+drop table EN_CASA_ANDABA.Consumibles
+drop table EN_CASA_ANDABA.Facturas
+drop table EN_CASA_ANDABA.MediosPago
+drop table EN_CASA_ANDABA.Tarjetas
+drop table EN_CASA_ANDABA.BajasHotel
+drop table EN_CASA_ANDABA.Hoteles_Usuarios
+drop table EN_CASA_ANDABA.Usuarios_ReservasCancelaciones
+drop table EN_CASA_ANDABA.Usuarios_ReservasModificaciones
+drop table EN_CASA_ANDABA.Regimenes_Hoteles
+drop table EN_CASA_ANDABA.Funcionalidades_Roles
+drop table EN_CASA_ANDABA.Funcionalidades
+drop table EN_CASA_ANDABA.Clientes_Estadias
+drop table EN_CASA_ANDABA.Reservas_Habitaciones
+drop table EN_CASA_ANDABA.Habitaciones
+drop table EN_CASA_ANDABA.Hoteles
+drop table EN_CASA_ANDABA.Roles_Usuarios
+drop table EN_CASA_ANDABA.Roles
+drop table EN_CASA_ANDABA.Estadias
+drop table EN_CASA_ANDABA.Reservas
+drop table EN_CASA_ANDABA.Usuarios
+drop table EN_CASA_ANDABA.Clientes
+drop table EN_CASA_ANDABA.ClientesErrores
+drop table EN_CASA_ANDABA.Regimenes
+drop table EN_CASA_ANDABA.Estados
+drop table EN_CASA_ANDABA.TiposHabitaciones
+drop table EN_CASA_ANDABA.Documentos
+PRINT 'Chau Tablas!'
+drop schema EN_CASA_ANDABA
+PRINT 'Chau Schema!'
+
+/* 
+--SELECTs Prueba
+select * from EN_CASA_ANDABA.Documentos
+select * from EN_CASA_ANDABA.Estados
+select * from EN_CASA_ANDABA.MediosPago
+select * from EN_CASA_ANDABA.Roles
+select * from EN_CASA_ANDABA.Funcionalidades
+select * from EN_CASA_ANDABA.Funcionalidades_Roles
+select * from EN_CASA_ANDABA.TiposHabitaciones
+select * from EN_CASA_ANDABA.Regimenes
+select * from EN_CASA_ANDABA.Consumibles
+select * from EN_CASA_ANDABA.Hoteles
+select * from EN_CASA_ANDABA.Habitaciones
+select * from EN_CASA_ANDABA.Facturas
+select * from EN_CASA_ANDABA.Items_Facturas
+select * from EN_CASA_ANDABA.Regimenes_Hoteles
+select * from #TemporalClientesRepetidos
+select * from EN_CASA_ANDABA.ClientesErrores
+select * from EN_CASA_ANDABA.Clientes
+select * from EN_CASA_ANDABA.Reservas
+select * from EN_CASA_ANDABA.Reservas_Habitaciones
+select * from EN_CASA_ANDABA.ReservasCancelaciones
+*/
