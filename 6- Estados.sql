ef9fce60d90e55d6375d267a9f78f15bc540a3a7
Federico <romanofederico5@gmail.com>
2018-06-09 15:40:54 -0300

    Deletes

--- 6- Estados.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Estados (estados_desc)
-VALUES
-/*1*/('Reserva Correcta'),
-/*2*/('Reserva Modificada'),
-/*3*/('Reserva Cancelada por Recepción'),
-/*4*/('Reserva Cancelada por Cliente'),
-/*5*/('Reserva Cancelada por No-Show'),
-/*6*/('Reserva Con Ingreso')
-GO
