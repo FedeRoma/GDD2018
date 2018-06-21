ebf873eb6f8235ba5336791184a5b53cd46829dd
GdeD <pablojmanteiga@gmail.com>
2018-06-21 19:39:05 -0300

    agrego maqueta ABMCliente MenuClientes

---
+++ src/2018/FrbaHotel/AbmCliente/MenuClientes.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FrbaHotel.ListadoEstadistico
+{
+    partial class ListadoEstadistico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMenuClientesAlta = new System.Windows.Forms.Button();
+            this.buttonMenuClientesModificacion = new System.Windows.Forms.Button();
+            this.buttonMenuClientesBaja = new System.Windows.Forms.Button();
+            this.buttonMenuClientesListado = new System.Windows.Forms.Button();
+            this.headerMenuClientes = new System.Windows.Forms.Label();
+            this.buttonMenuClientesAtras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMenuClientesAlta
+            // 
+            this.buttonMenuClientesAlta.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.buttonMenuClientesAlta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenuClientesAlta.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMenuClientesAlta.ForeColor = System.Drawing.Color.White;
+            this.buttonMenuClientesAlta.Location = new System.Drawing.Point(197, 148);
+            this.buttonMenuClientesAlta.Name = "buttonMenuClientesAlta";
+            this.buttonMenuClientesAlta.Size = new System.Drawing.Size(150, 150);
+            this.buttonMenuClientesAlta.TabIndex = 12;
+            this.buttonMenuClientesAlta.Text = "ALTA";
+            this.buttonMenuClientesAlta.UseVisualStyleBackColor = false;
+            // 
+            // buttonMenuClientesModificacion
+            // 
+            this.buttonMenuClientesModificacion.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.buttonMenuClientesModificacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenuClientesModificacion.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMenuClientesModificacion.ForeColor = System.Drawing.Color.White;
+            this.buttonMenuClientesModificacion.Location = new System.Drawing.Point(197, 304);
+            this.buttonMenuClientesModificacion.Name = "buttonMenuClientesModificacion";
+            this.buttonMenuClientesModificacion.Size = new System.Drawing.Size(306, 84);
+            this.buttonMenuClientesModificacion.TabIndex = 15;
+            this.buttonMenuClientesModificacion.Text = "MODIFICACION";
+            this.buttonMenuClientesModificacion.UseVisualStyleBackColor = false;
+            // 
+            // buttonMenuClientesBaja
+            // 
+            this.buttonMenuClientesBaja.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.buttonMenuClientesBaja.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenuClientesBaja.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMenuClientesBaja.ForeColor = System.Drawing.Color.White;
+            this.buttonMenuClientesBaja.Location = new System.Drawing.Point(353, 148);
+            this.buttonMenuClientesBaja.Name = "buttonMenuClientesBaja";
+            this.buttonMenuClientesBaja.Size = new System.Drawing.Size(150, 150);
+            this.buttonMenuClientesBaja.TabIndex = 16;
+            this.buttonMenuClientesBaja.Text = "BAJA";
+            this.buttonMenuClientesBaja.UseVisualStyleBackColor = false;
+            // 
+            // buttonMenuClientesListado
+            // 
+            this.buttonMenuClientesListado.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.buttonMenuClientesListado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenuClientesListado.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMenuClientesListado.ForeColor = System.Drawing.Color.White;
+            this.buttonMenuClientesListado.Location = new System.Drawing.Point(197, 58);
+            this.buttonMenuClientesListado.Name = "buttonMenuClientesListado";
+            this.buttonMenuClientesListado.Size = new System.Drawing.Size(306, 84);
+            this.buttonMenuClientesListado.TabIndex = 17;
+            this.buttonMenuClientesListado.Text = "LISTADO";
+            this.buttonMenuClientesListado.UseVisualStyleBackColor = false;
+            // 
+            // headerMenuClientes
+            // 
+            this.headerMenuClientes.BackColor = System.Drawing.Color.Transparent;
+            this.headerMenuClientes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerMenuClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.headerMenuClientes.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerMenuClientes.ForeColor = System.Drawing.Color.DimGray;
+            this.headerMenuClientes.Location = new System.Drawing.Point(0, 0);
+            this.headerMenuClientes.Name = "headerMenuClientes";
+            this.headerMenuClientes.Size = new System.Drawing.Size(700, 51);
+            this.headerMenuClientes.TabIndex = 18;
+            this.headerMenuClientes.Text = "CLIENTES";
+            this.headerMenuClientes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonMenuClientesAtras
+            // 
+            this.buttonMenuClientesAtras.BackColor = System.Drawing.Color.Crimson;
+            this.buttonMenuClientesAtras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenuClientesAtras.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMenuClientesAtras.ForeColor = System.Drawing.Color.White;
+            this.buttonMenuClientesAtras.Location = new System.Drawing.Point(12, 388);
+            this.buttonMenuClientesAtras.Name = "buttonMenuClientesAtras";
+            this.buttonMenuClientesAtras.Size = new System.Drawing.Size(89, 38);
+            this.buttonMenuClientesAtras.TabIndex = 22;
+            this.buttonMenuClientesAtras.Text = "Atrás";
+            this.buttonMenuClientesAtras.UseVisualStyleBackColor = false;
+            // 
+            // ListadoEstadistico
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(700, 438);
+            this.Controls.Add(this.buttonMenuClientesAtras);
+            this.Controls.Add(this.headerMenuClientes);
+            this.Controls.Add(this.buttonMenuClientesListado);
+            this.Controls.Add(this.buttonMenuClientesBaja);
+            this.Controls.Add(this.buttonMenuClientesModificacion);
+            this.Controls.Add(this.buttonMenuClientesAlta);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ListadoEstadistico";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRBA HOTEL 2018";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ATRASClientes;
+        private System.Windows.Forms.Button buttonMenuClientesAlta;
+        private System.Windows.Forms.Button buttonMenuClientesModificacion;
+        private System.Windows.Forms.Button buttonMenuClientesBaja;
+        private System.Windows.Forms.Button buttonMenuClientesListado;
+        private System.Windows.Forms.Label headerCLIENTES;
+        private System.Windows.Forms.Label headerMenuClientes;
+        private System.Windows.Forms.Button buttonMenuClientesAtras;
+    }
+}
