d1dded8dd9c428eae89c6b32018759bc2d99c9c8
GdeD <pablojmanteiga@gmail.com>
2018-06-25 15:51:00 -0300

    auxiliar de querys de la interfaz

--- data/querys_interfaz.sql
+++ data/querys_interfaz.sql
@@ -5,6 +5,12 @@ from EN_CASA_ANDABA.Usuarios
 where usu_username = 'admin' and usu_password = hashbytes('SHA2_256', 'w23e')
 -- oK!
 
+
+select count (*) ryu_rol_id 
+from EN_CASA_ANDABA.Roles_Usuarios 
+where ryu_usu_id = 1
+-- oK!
+
 select fyr_fun_id
 from EN_CASA_ANDABA.Funcionalidades_Roles
 where fyr_rol_id = (select ryu_rol_id 
