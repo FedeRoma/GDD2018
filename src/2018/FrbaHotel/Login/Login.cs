92133d33aceb2b37dd0c929c4b104cef2349e3c7
GdeD <pablojmanteiga@gmail.com>
2018-06-25 15:53:31 -0300

    update Login

--- src/2018/FrbaHotel/Login/Login.cs
+++ src/2018/FrbaHotel/Login/Login.cs
@@ -32,43 +32,53 @@ namespace FrbaHotel.Login
         {
             cantIntentos++;
             SqlDataReader resultado;
-            resultado = Index.BD.comando("SELECT usu_estado, usu_id FROM EN_CASA_ANDABA.Usuarios where usu_username = '" + nombreUsuario.Text + "'and usu_password = '" + getHash(password.Text) + "'");
+            resultado = Index.BD.comando("select usu_estado, usu_id from EN_CASA_ANDABA.Usuarios where usu_username = '"+ nombreUsuario.Text +"' and usu_password = hashbytes('SHA2_256', '"+ password.Text +"')");
+            
             if (nombreUsuario.Text == "guest" || nombreUsuario.Text == "Guest")
             {
-                MessageBox.Show("Continuando como Invitado...");
+                MessageBox.Show("Continuando como invitado...");
                 funcionalidadesUsuarios = new FuncionalidadesUsuarios();
                 funcionalidadesUsuarios.Show();
                 this.Hide();
             }
-            else if (resultado.Read() == true && cantIntentos <= 4)
+            else
             {
-                if (resultado.GetBoolean(0) == true)
+                if (resultado.Read() == true && cantIntentos <= 4)
                 {
-                    string mensaje = "BIENVENIDO" + nombreUsuario.Text;
-                    MessageBox.Show(mensaje);
-                    usuarioId = resultado.GetInt32(1);
-                    cantIntentos = 0;
-                    resultado.Close();
-
-                    string consulta = "select ryu_rol_id from EN_CASA_ANDABA.Roles_Usuarios where ryu_usu_id = " + usuarioId + ";";
-
-                    resultado = Index.BD.comando(consulta);
-                    resultado.Read();
-                    FrbaHotel.Index.rol = resultado.GetInt32(0);
-                    resultado.Close();
-                    if (rol > 0)
+                    if (resultado.GetBoolean(0) == true)
                     {
-                        funcionalidadesUsuarios = new FuncionalidadesUsuarios();
-                        funcionalidadesUsuarios.Show();
-                        this.Hide();
-                    }
-                    if (rol == 1)
-                    {
-                        // #Issue01
+                        string mensaje = "BIENVENIDO " + nombreUsuario.Text;
+                        MessageBox.Show(mensaje);
+                        usuarioId = resultado.GetInt32(1);
+                        cantIntentos = 0;
+                        resultado.Close();
+
+                        resultado = Index.BD.comando("select count (*) ryu_rol_id from EN_CASA_ANDABA.Roles_Usuarios where ryu_usu_id = " + usuarioId + "");
+                        resultado.Read();
+                        rol = resultado.GetInt32(0);
+                        resultado.Close();
+
+                        if (rol > 0)
+                        {
+                            
+                        }
+                        if (rol == 1)
+                        {
+                            funcionalidadesUsuarios = new FuncionalidadesUsuarios();
+                            funcionalidadesUsuarios.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("#error: El usuario seleccionado no tiene ningún Rol asignado");
+                            nombreUsuario.Clear();
+                            password.Clear();
+                            nombreUsuario.Focus();
+                        }
                     }
                     else
                     {
-                        MessageBox.Show("Error: El usuario seleccionado no tiene ningún Rol asignado");
+                        MessageBox.Show("Error: El usuario seleccionado se encuentra inhabilitado");
                         nombreUsuario.Clear();
                         password.Clear();
                         nombreUsuario.Focus();
@@ -76,42 +86,20 @@ namespace FrbaHotel.Login
                 }
                 else
                 {
-                    MessageBox.Show("Error: El usuario seleccionado se encuentra inhabilitado");
+                    MessageBox.Show("#error: Nombre de Usuario o Contraseña incorrectos");
                     nombreUsuario.Clear();
                     password.Clear();
                     nombreUsuario.Focus();
+                    if (cantIntentos > 3)
+                    {
+                        MessageBox.Show("#error: Ha sobrepasado la cantidad de intentos fallidos");
+                        this.Hide();
+                        index = new Index();
+                        index.Show();
+                    }
                 }
+                resultado.Close();
             }
-            else
-            {
-                MessageBox.Show("Error: Datos incorrectos");
-                nombreUsuario.Clear();
-                password.Clear();
-                nombreUsuario.Focus();
-                if (cantIntentos > 3)
-                {
-                    MessageBox.Show("Error: Ha sobrepasado la cantidad de intentos fallidos");
-                    this.Hide();
-                    index = new Index();
-                    index.Show();
-                }
-            }
-            resultado.Close();
-
-        }
-
-        public static string getHash(string password)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(password);
-
-            System.Security.Cryptography.SHA256Managed sha256hashstring = new System.Security.Cryptography.SHA256Managed();
-            byte[] hash = sha256hashstring.ComputeHash(bytes);
-            string hashstring = string.Empty;
-            foreach (byte x in hash)
-            {
-                hashstring += String.Format("{0:x2}", x);
-            }
-            return hashstring;
         }
 
         private void cancelar_Click(object sender, EventArgs e)
