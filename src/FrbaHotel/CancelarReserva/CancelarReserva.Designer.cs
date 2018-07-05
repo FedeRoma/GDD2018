42bcf9cd25644631f80b7f2d64e40917be917007
GdeD <pablojmanteiga@gmail.com>
2018-07-05 06:15:03 -0300

    CancelarReserva Ok!

---
+++ src/FrbaHotel/CancelarReserva/CancelarReserva.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace FrbaHotel.CancelarReserva
+{
+    partial class CancelarReserva
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CancelarReserva));
+            this.label1 = new System.Windows.Forms.Label();
+            this.codigoReserva = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.motivoCancelacion = new System.Windows.Forms.TextBox();
+            this.aceptar = new System.Windows.Forms.Button();
+            this.atras = new System.Windows.Forms.Button();
+            this.limpiar = new System.Windows.Forms.Button();
+            this.fechaCancelacion = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(186, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 41);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CANCELAR RESERVA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // codigoReserva
+            // 
+            this.codigoReserva.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codigoReserva.ForeColor = System.Drawing.Color.DimGray;
+            this.codigoReserva.Location = new System.Drawing.Point(151, 109);
+            this.codigoReserva.Name = "codigoReserva";
+            this.codigoReserva.Size = new System.Drawing.Size(406, 22);
+            this.codigoReserva.TabIndex = 1;
+            this.codigoReserva.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.codigoReserva_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(148, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Código de Reserva";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(148, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 14);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Motivo de la Cancelación";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // motivoCancelacion
+            // 
+            this.motivoCancelacion.Location = new System.Drawing.Point(151, 151);
+            this.motivoCancelacion.Multiline = true;
+            this.motivoCancelacion.Name = "motivoCancelacion";
+            this.motivoCancelacion.Size = new System.Drawing.Size(406, 158);
+            this.motivoCancelacion.TabIndex = 2;
+            // 
+            // aceptar
+            // 
+            this.aceptar.BackColor = System.Drawing.Color.Crimson;
+            this.aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aceptar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aceptar.ForeColor = System.Drawing.Color.White;
+            this.aceptar.Location = new System.Drawing.Point(591, 386);
+            this.aceptar.Name = "aceptar";
+            this.aceptar.Size = new System.Drawing.Size(101, 44);
+            this.aceptar.TabIndex = 4;
+            this.aceptar.Text = "aceptar";
+            this.aceptar.UseVisualStyleBackColor = false;
+            this.aceptar.Click += new System.EventHandler(this.aceptar_Click);
+            // 
+            // atras
+            // 
+            this.atras.BackColor = System.Drawing.Color.DimGray;
+            this.atras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.atras.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atras.ForeColor = System.Drawing.Color.White;
+            this.atras.Location = new System.Drawing.Point(12, 386);
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(101, 44);
+            this.atras.TabIndex = 6;
+            this.atras.Text = "atrás";
+            this.atras.UseVisualStyleBackColor = false;
+            this.atras.Click += new System.EventHandler(this.atras_Click);
+            // 
+            // limpiar
+            // 
+            this.limpiar.BackColor = System.Drawing.Color.White;
+            this.limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.limpiar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limpiar.ForeColor = System.Drawing.Color.DimGray;
+            this.limpiar.Location = new System.Drawing.Point(119, 386);
+            this.limpiar.Name = "limpiar";
+            this.limpiar.Size = new System.Drawing.Size(101, 44);
+            this.limpiar.TabIndex = 5;
+            this.limpiar.Text = "limpiar datos";
+            this.limpiar.UseVisualStyleBackColor = false;
+            this.limpiar.Click += new System.EventHandler(this.limpiar_Click);
+            // 
+            // fechaCancelacion
+            // 
+            this.fechaCancelacion.CalendarTitleBackColor = System.Drawing.Color.Crimson;
+            this.fechaCancelacion.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fechaCancelacion.Location = new System.Drawing.Point(151, 329);
+            this.fechaCancelacion.Name = "fechaCancelacion";
+            this.fechaCancelacion.Size = new System.Drawing.Size(406, 21);
+            this.fechaCancelacion.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(148, 312);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 14);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Fecha";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // CancelarReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(704, 442);
+            this.Controls.Add(this.fechaCancelacion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.atras);
+            this.Controls.Add(this.limpiar);
+            this.Controls.Add(this.aceptar);
+            this.Controls.Add(this.motivoCancelacion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.codigoReserva);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CancelarReserva";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRBA Hotel 2018";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox codigoReserva;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox motivoCancelacion;
+        private System.Windows.Forms.Button aceptar;
+        private System.Windows.Forms.Button atras;
+        private System.Windows.Forms.Button limpiar;
+        private System.Windows.Forms.DateTimePicker fechaCancelacion;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ src/FrbaHotel/CancelarReserva/CancelarReserva.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FrbaHotel.CancelarReserva
+{
+    public partial class CancelarReserva : Form
+    {
+        public static Login.MenuFuncionalidades menuFuncionalidades;
+        SqlDataReader qry;
+        int noshowCancID = 0;
+        int usuCancID = 0;
+        int cliCancID = 0;
+        DateTime fechaCanc, hoy;
+
+        public CancelarReserva()
+        {
+            InitializeComponent();
+            codigoReserva.Clear();
+            codigoReserva.Focus();
+
+            qry = Index.BD.consultaGetPuntero("select estados_id from EN_CASA_ANDABA.Estados where estados_desc = 'RESERVA CANCELADA POR NO-SHOW'");
+            qry.Read();
+            noshowCancID = qry.GetInt32(0);
+            qry.Close();
+
+            qry = Index.BD.consultaGetPuntero("select estados_id from EN_CASA_ANDABA.Estados where estados_desc = 'RESERVA CANCELADA POR RECEPCION'");
+            qry.Read();
+            usuCancID = qry.GetInt32(0);
+            qry.Close();
+
+            qry = Index.BD.consultaGetPuntero("select estados_id from EN_CASA_ANDABA.Estados where estados_desc = 'RESERVA CANCELADA POR CLIENTE'");
+            qry.Read();
+            cliCancID = qry.GetInt32(0);
+            qry.Close();
+        }
+
+        private void codigoReserva_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool checkCampos()
+        {
+            bool inconsistencias = false;
+            string alerta = "";
+
+            if (string.IsNullOrEmpty(codigoReserva.Text))
+            {
+                alerta = alerta + "Debe ingresar un código de reserva válido\n";
+                inconsistencias = true;
+            }
+
+            fechaCanc = Convert.ToDateTime(fechaCancelacion.Value);
+            hoy = DateTime.Today;
+
+            if (DateTime.Compare(fechaCanc, hoy) >= 0)
+            {
+                alerta = alerta + "Debe ingresar una fecha de cancelación válida";
+                inconsistencias = true;
+            }
+
+            if (inconsistencias)
+            {
+                MessageBox.Show(alerta);
+            }
+            return inconsistencias;
+        }
+
+        private void aceptar_Click(object sender, EventArgs e)
+        {
+            if (!checkCampos())
+            {
+                qry = Index.BD.consultaGetPuntero("select res_estados_id, res_inicio from EN_CASA_ANDABA.Reservas where res_id = " + codigoReserva.Text);
+                if (qry.Read())
+                {
+                    int estado = qry.GetInt32(0);
+                    DateTime fechaInicio = qry.GetDateTime(1);
+                    qry.Close();
+
+                    if (fechaInicio.Date <= fechaCancelacion.Value)
+                    {
+                        MessageBox.Show("La reserva no puede cancelarse ya que se encuentra dentro del plazo mínino de cancelación");
+                        return;
+                    }
+                    if (estado == noshowCancID || estado == cliCancID || estado == usuCancID)
+                    {
+                        MessageBox.Show("La reserva ya ha sido cancelada con anterioridad");
+                        return;
+                    }
+
+                    qry = Index.BD.consultaGetPuntero("exec EN_CASA_ANDABA.bajaReserva " + codigoReserva.Text + ", " + Index.usuarioID + ", " + estado + ", '" + fechaCanc.Date.ToString("yyyyMMdd HH:mm:ss") + "', '" + motivoCancelacion.Text + "'");
+                    if (qry.Read())
+                    {
+                        if (qry.GetInt32(0) == 1)
+                        {
+                            MessageBox.Show("La reserva se ha cancelado con éxito");
+                            return;
+                        }
+                        else
+                        {
+                            MessageBox.Show("#error: no se ha podido realizar la operación");
+                            return;
+                        }
+                    }
+                    qry.Close();
+                }
+                qry.Close();
+            }
+            else
+            {
+                MessageBox.Show("#error: no se ha encontrado una reserva con ese número");
+                return;
+            }
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            codigoReserva.ResetText();
+            motivoCancelacion.ResetText();
+            fechaCancelacion.ResetText();
+            codigoReserva.Focus();
+        }
+
+        private void atras_Click(object sender, EventArgs e)
+        {
+            menuFuncionalidades = new Login.MenuFuncionalidades();
+            menuFuncionalidades.Show();
+            this.Close();
+        }
+
+    }
+}
