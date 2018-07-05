94a9a6c018f6d0ffe7a89f954928e5d5c4843e44
GdeD <pablojmanteiga@gmail.com>
2018-07-05 19:00:55 -0300

    subo la base de los formularios que faltaban

--- src/FrbaHotel/RegistrarEstadia/CheckIn.cs
+++ src/FrbaHotel/RegistrarEstadia/CheckIn.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FrbaHotel.AbmRegimen
+namespace FrbaHotel.RegistrarEstadia
 {
-    public partial class Form1 : Form
+    public partial class CheckIn : Form
     {
-        public Form1()
+        public CheckIn()
         {
             InitializeComponent();
         }
