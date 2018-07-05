7c91673e30ee9a272a3e00e2fd45c8099509d722
GdeD <pablojmanteiga@gmail.com>
2018-07-05 16:57:30 -0300

    agrego algunas pantallas

---
+++ src/FrbaHotel/RegistrarEstadia/MenuRegistrarEstadia.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FrbaHotel.RegistrarEstadia
+{
+    partial class MenuRegistrarEstadia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuRegistrarEstadia));
+            this.atras = new System.Windows.Forms.Button();
+            this.checkOut = new System.Windows.Forms.Button();
+            this.checkIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // atras
+            // 
+            this.atras.BackColor = System.Drawing.Color.DimGray;
+            this.atras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.atras.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atras.ForeColor = System.Drawing.Color.White;
+            this.atras.Location = new System.Drawing.Point(12, 386);
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(101, 44);
+            this.atras.TabIndex = 3;
+            this.atras.Text = "atrás";
+            this.atras.UseVisualStyleBackColor = false;
+            // 
+            // checkOut
+            // 
+            this.checkOut.BackColor = System.Drawing.Color.Crimson;
+            this.checkOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkOut.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkOut.ForeColor = System.Drawing.Color.White;
+            this.checkOut.Location = new System.Drawing.Point(355, 155);
+            this.checkOut.Name = "checkOut";
+            this.checkOut.Size = new System.Drawing.Size(156, 131);
+            this.checkOut.TabIndex = 2;
+            this.checkOut.Text = "Check Out";
+            this.checkOut.UseVisualStyleBackColor = false;
+            // 
+            // checkIn
+            // 
+            this.checkIn.BackColor = System.Drawing.Color.Crimson;
+            this.checkIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkIn.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkIn.ForeColor = System.Drawing.Color.White;
+            this.checkIn.Location = new System.Drawing.Point(193, 155);
+            this.checkIn.Name = "checkIn";
+            this.checkIn.Size = new System.Drawing.Size(156, 131);
+            this.checkIn.TabIndex = 1;
+            this.checkIn.Text = "Check In";
+            this.checkIn.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 41);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ADMINISTRAR ESTADIAS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MenuRegistrarEstadia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(704, 442);
+            this.Controls.Add(this.atras);
+            this.Controls.Add(this.checkOut);
+            this.Controls.Add(this.checkIn);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MenuRegistrarEstadia";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRBA Hotel 2018";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button atras;
+        private System.Windows.Forms.Button checkOut;
+        private System.Windows.Forms.Button checkIn;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- src/FrbaHotel/RegistrarEstadia/MenuRegistrarEstadia.cs
+++ src/FrbaHotel/RegistrarEstadia/MenuRegistrarEstadia.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FrbaHotel.RegistrarEstadia
 {
-    public partial class Form1 : Form
+    public partial class MenuRegistrarEstadia : Form
     {
-        public Form1()
+        public MenuRegistrarEstadia()
         {
             InitializeComponent();
         }
