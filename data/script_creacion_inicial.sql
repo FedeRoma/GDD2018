70418b16d4214807b93014492811a5a3dd5d31b2
GdeD <pablojmanteiga@gmail.com>
2018-07-13 02:37:14 -0300

    update script inicial (

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -706,27 +706,6 @@ create procedure EN_CASA_ANDABA.modificacionClientes_Estadias
 	end
 go
 
-create procedure EN_CASA_ANDABA.altaTarjeta 
-	@facturaId int, @banco varchar(50), @emisor varchar(50), @numero bigint, @titular varchar(50) as
-	begin
-		declare @resultado bit
-		begin tran tAltaTarjeta
-		begin try
-			insert into EN_CASA_ANDABA.Tarjetas(tar_emisor, tar_numero, tar_banco, tar_titular) 
-			values (@emisor, @numero, @banco, @titular)
-			UPDATE EN_CASA_ANDABA.Facturas set fac_tar_id = (select SCOPE_IDENTITY()) where fac_id = @facturaId
-			set @resultado = 1
-			select @resultado as resultado
-			commit tran tAltaTarjeta
-		end try
-		begin catch
-			rollback tran tAltaTarjeta
-			set @resultado = 0
-			select @resultado as resultado
-		end catch
-	end
-go
-
 create procedure EN_CASA_ANDABA.altaCheckInEstadia
 	@reservaId int, @usuarioId int, @fechaI date as
 	begin
@@ -867,6 +846,41 @@ create procedure EN_CASA_ANDABA.modificacionFactura
 			rollback tran tModificacionFactura
 			set @resultado = 0
 			select @resultado as resultado
+		end catch
+	end
+go
+
+create procedure EN_CASA_ANDABA.RegistrarConsumbleEstadia
+	@estadia numeric(18,0), @cons numeric(18,0), @hab numeric(18,0) as
+	begin
+		declare @respuesta numeric(18,0), @item numeric(18,0), @descri nvarchar(255), @factu numeric(18,0),
+			@precio numeric(18,0)
+	begin tran ta
+		begin try
+			set @precio = (select con_precio from EN_CASA_ANDABA.Consumibles where con_id = @cons)
+			set @descri = (	select con_desc from EN_CASA_ANDABA.Consumibles where con_id = @cons)
+			set @factu = (select fac_id from EN_CASA_ANDABA.Facturas where fac_est_res_id = @estadia)
+			if( exists(select iyf_id from EN_CASA_ANDABA.Items_Facturas where  iyf_fac_id = @factu))
+				begin
+					set @item = (select iyf_id from EN_CASA_ANDABA.Items_Facturas where  iyf_fac_id = @factu)
+					update EN_CASA_ANDABA.Items_Facturas 
+					set iyf_monto = iyf_monto + @precio, iyf_cantidad = iyf_cantidad + 1
+					where iyf_id=@item
+				end
+			else
+				begin
+					insert into EN_CASA_ANDABA.Items_Facturas(iyf_fac_id,iyf_cantidad,iyf_monto)
+					values(@factu,1,@precio,@descri)
+					set @item = (select iyf_id from EN_CASA_ANDABA.Items_Facturas where iyf_fac_id = @factu)
+				end
+				set @respuesta = 1
+				select @respuesta as respuesta
+				commit tran ta
+		end try
+		begin catch
+			rollback tran ta
+			set @respuesta = 0
+			select @respuesta as respuesta
 		end catch
 	end
 go
